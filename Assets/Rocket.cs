409df4736e750c2fba4dc2e961501c26683bdc60
Sean Ryan <sryan@evercrack.com>
2018-04-28 13:23:17 -0400

    Tweak mass for rocket to take off.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,30 +5,32 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    Rigidbody rigidBody;
+
 	// Use this for initialization
 	void Start () {
-		
+        rigidBody = GetComponent<Rigidbody>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
         ProcessInput();
-		
+
 	}
 
     private void ProcessInput()
     {
         if( Input.GetKey(KeyCode.Space))
         {
-            print("Thrust"); 
+            rigidBody.AddRelativeForce(Vector3.up);
         }
 
         if ( Input.GetKey(KeyCode.A))
         {
-            print("Rotate Left");  
+            print("Rotate Left");
         } else if( Input.GetKey(KeyCode.D))
         {
-            print("Rotate Right"); 
+            print("Rotate Right");
         }
     }
 }
