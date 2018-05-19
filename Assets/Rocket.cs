ca97bfeec75afba84c496f5b5112769c89773fa3
Sean Ryan <sryan@evercrack.com>
2018-05-19 13:43:07 -0400

    Lecture 4: add sound on thrust

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,31 +6,55 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
+    bool m_PlayThrust = false;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 
 	// Update is called once per frame
 	void Update () {
         ProcessInput();
-
+        PlaySounds();
 	}
+
+
+    private void PlaySounds()
+    {
+        // We don't want to re-play if the clip is always playing.
+        if ( m_PlayThrust == true && ! audioSource.isPlaying )
+        {
+            audioSource.Play();
+        }
+
+        if ( m_PlayThrust == false && audioSource.isPlaying )
+        {
+            audioSource.Stop();
+        }
+
+    }
 
     private void ProcessInput()
     {
         if( Input.GetKey(KeyCode.Space))
         {
+            m_PlayThrust = true;
             rigidBody.AddRelativeForce(Vector3.up);
+        }
+        else
+        {
+            m_PlayThrust = false;
         }
 
         if ( Input.GetKey(KeyCode.A))
         {
-            print("Rotate Left");
+            transform.Rotate(Vector3.forward);
         } else if( Input.GetKey(KeyCode.D))
         {
-            print("Rotate Right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
